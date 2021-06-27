e7303a2a424197c6e3d2e4e1b48fc40c12eb2469
Andrey Shkulipa <83605295+Shkulipa13@users.noreply.github.com>
2021-06-27 23:52:13 +0300

    IIS website vs picture replacement

---
+++ iiswebsite.ps1
@@ -0,0 +1,8 @@
+﻿# Install server
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
+
+#Module import
+Import-Module -Name WebAdministration
+
+#Replacing image
+Copy-Item -Path "C:\inetpub\shalomteam\iisstart.png" -Destination "C:\inetpub\wwwroot\iisstart.png" -Force
