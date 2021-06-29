dadffa8043447e207339aa33b1bb12ec54647933
Shkulipa13 <83605295+Shkulipa13@users.noreply.github.com>
2021-06-29 20:41:09 +0300

    Update iiswebsite.ps1
    DZ2 health check code!

--- iiswebsite.ps1
+++ iiswebsite.ps1
@@ -5,4 +5,9 @@ Install-WindowsFeature -name Web-Server -IncludeManagementTools
 Import-Module -Name WebAdministration
 
 #Replacing image
-Copy-Item -Path "C:\inetpub\shalomteam\iisstart.png" -Destination "C:\inetpub\wwwroot\iisstart.png" -Force
+Copy-Item -Path "C:\Windows\Web\Screen\img103.png" -Destination "C:\inetpub\wwwroot\iisstart.png" -Force
+
+#DZ2 health check code
+$ie = new-object -com InternetExplorer.Application
+$ie.Visible = $true
+$ie.Navigate("http://localhost/");
